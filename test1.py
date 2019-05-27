8c50aaa8ddc6380d57dce79751156db136a83e8a
Ghasak Ibrahim <mdotgh@gmail.com>
2019-05-27 16:27:22 +0900

    added teh make_blobs generator function

--- test1.py
+++ test1.py
@@ -5,3 +5,14 @@ for i in (listA):
 
 # How to get the reverse of a list:
 print(listA[::-1])
+
+
+import numpy as np
+from sklearn.datasets.samples_generator import make_blobs
+
+X, y = make_blobs(n_samples=15, centers=3, n_features=2)
+
+print(X)
+print(X[1])
+print(X[1,1])
+print(X[1][1])
